6db6964653e0e68263605aa6e2ddb96313e7e25e
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-19 07:38:03 +0200

    end-part-28

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -16,11 +16,21 @@ namespace EmployeeManagement.Web.Pages
         public IEnumerable<Employee> Employees { get; set; }
         public bool ShowFooter { get; set; } = true;
 
+        public int SelectedEmployeesCount { get; set; }
         protected override async Task OnInitializedAsync()
         {
             Employees = (await EmployeeService.GetEmployees()).ToList();
         }
 
-
+        public void EmployeeSelectionChanged(bool isSelected) {
+           if (isSelected)
+           {
+               SelectedEmployeesCount++;
+           }
+           else
+           {
+               SelectedEmployeesCount--;
+           }
+        }
     }
 }
